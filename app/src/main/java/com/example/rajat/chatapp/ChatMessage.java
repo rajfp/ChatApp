887b054f5b1cb2ee9d2abe716ea7c9a8a33985d8
rajat <rajatnfp@gmail.com>
2017-09-19 21:21:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rajat/chatapp/ChatMessage.java
@@ -0,0 +1,46 @@
+package com.example.rajat.chatapp;
+
+import java.util.Date;
+
+/**
+ * Created by Rajat on 28-Jun-17.
+ */
+public class ChatMessage {
+    private String messageText;
+    private String messageUser;
+    private long messagetime;
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+        messagetime=new Date().getTime();
+    }
+    public  ChatMessage()
+    {
+
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessagetime() {
+        return messagetime;
+    }
+
+    public void setMessagetime(long messagetime) {
+        this.messagetime = messagetime;
+    }
+}
